f87836a55e7af037f9c908bae6abc8addb69f88f
jinwen11 <654915277@qq.com>
2016-11-21 23:25:21 +0800

    2

--- CEA_CR.PlatForm/Utils/HttpDataService.cs
+++ CEA_CR.PlatForm/Utils/HttpDataService.cs
@@ -20,18 +20,18 @@ namespace CEA_CR.PlatForm.Utils
                 return result;
             }
 
-            #region 测试数据
-            result.Add(new CurrentCourseItem { classId = "354", className = "新雇员训练1671（本部）", courseId = "3", courseName = "各种机型设备理论", time = "2016-10-11 07:30" });
-            result.Add(new CurrentCourseItem { classId = "354", className = "新雇员训练1671（本部）", courseId = "33", courseName = "紧急医学事件处置训练理论", time = "2016-10-11 15:30" });
-            return result;
-            #endregion
+            //#region 测试数据
+            //result.Add(new CurrentCourseItem { classId = "354", className = "新雇员训练1671（本部）", courseId = "3", courseName = "各种机型设备理论", time = "2016-10-11 07:30" });
+            //result.Add(new CurrentCourseItem { classId = "354", className = "新雇员训练1671（本部）", courseId = "33", courseName = "紧急医学事件处置训练理论", time = "2016-10-11 15:30" });
+            //return result;
+            //#endregion
 
             string cacheKey = string.Format("course:{0}", classRoomId);
             result = CacheHelper.CacheManager.Get<List<CurrentCourseItem>>(cacheKey);
             if (result == null || result.Count == 0)
             {
                 string responseJson = HttpHelper.GetHttpResponse(string.Format(ConfigStatic.GetCurrentCourseUrl, ConfigStatic.userName, ConfigStatic.password, classRoomId));
-
+                
                 if (true)
                 {
                     var response = JsonConvert.DeserializeObject<CurrentCourseResponse>(responseJson);
@@ -44,7 +44,7 @@ namespace CEA_CR.PlatForm.Utils
                 {
                     using (StringReader rdr = new StringReader(responseJson))
                     {
-                        XmlSerializer serializer = new XmlSerializer(typeof(List<CurrentCourseItem>));
+                        XmlSerializer serializer = new XmlSerializer(typeof(CurrentCourseResponse));
                         var response = (CurrentCourseResponse)serializer.Deserialize(rdr);
                         if (response != null)
                         {
@@ -70,11 +70,11 @@ namespace CEA_CR.PlatForm.Utils
                 return result;
             }
 
-            #region 测试数据
-            result.Add(new CourseScheduleItem { classId = "27305333", className = "新雇员训练1671（本部）", courseId = "734", courseName = "机上服务标准与程序操作", place = "710", t_date = "2016-10-29", time = "07:30", roomNo = "JS-050" });
-            result.Add(new CourseScheduleItem { classId = "27305333", className = "新雇员训练1671（本部）", courseId = "610", courseName = "旅客特殊服务", place = "550", t_date = "2016-10-20", time = "14:30", roomNo = "JS-030" });
-            return result;
-            #endregion
+            //#region 测试数据
+            //result.Add(new CourseScheduleItem { classId = "27305333", className = "新雇员训练1671（本部）", courseId = "734", courseName = "机上服务标准与程序操作", place = "710", t_date = "2016-10-29", time = "07:30", roomNo = "JS-050" });
+            //result.Add(new CourseScheduleItem { classId = "27305333", className = "新雇员训练1671（本部）", courseId = "610", courseName = "旅客特殊服务", place = "550", t_date = "2016-10-20", time = "14:30", roomNo = "JS-030" });
+            //return result;
+            //#endregion
 
             string cacheKey = string.Format("schedule:{0}-{1}-{2}", identityCard, month, userType);
             result = CacheHelper.CacheManager.Get<List<CourseScheduleItem>>(cacheKey);
